0396ce4848472b4bc1d012808f8052d874ccb496
irensula <irinawebdev@gmail.com>
2025-09-18 15:16:05 +0300

    map shows markers (not callouts)

--- RNMaps/components/Map.js
+++ RNMaps/components/Map.js
@@ -1,17 +1,46 @@
-import MapView from "react-native-maps";
-import { StyleSheet, Text, View } from "react-native";
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import { MarkersList } from "./MarkersList";
+//In your android/gradle.properties, chnage newArchEnabled=true to newArchEnabled=false
 
 const Map = () => {
+  const INITIAL_REGION = {
+    latitude: 61.50134,
+    longitude: 23.7465,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.05,
+  };
+  // const onMarkerSelected = (marker) => {
+  //   Alert.alert(marker.name);
+  // };
   return (
     <MapView
       style={styles.map}
-      initialRegion={{
-        latitude: 37.78825, // пример: Сан-Франциско
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
+      // provider={PROVIDER_GOOGLE}
+      initialRegion={INITIAL_REGION}
+      showsUserLocation
+      showsMyLocationButton
+    >
+      {MarkersList.map((marker, index) => (
+        <Marker
+          key={index}
+          coordinate={{
+            latitude: marker.latitude,
+            longitude: marker.longitude,
+          }}
+          onPress={() => setSelectedMarker(marker)}
+          // onPress={() => onMarkerSelected(marker)}
+        >
+          <Callout>
+            <View
+              style={{ padding: 10, backgroundColor: "white", borderRadius: 8 }}
+            >
+              <Text style={{ fontWeight: "bold" }}>{marker.name}</Text>
+            </View>
+          </Callout>
+        </Marker>
+      ))}
+    </MapView>
   );
 };
 
@@ -19,6 +48,18 @@ const styles = StyleSheet.create({
   map: {
     width: "100%",
     height: "100%",
+  },
+  callout: {
+    width: 120, // must have width
+    padding: 10,
+    backgroundColor: "white",
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  calloutText: {
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
 
