0c1c19b51f5e399e3d125b41d48b443ff90f7228
irensula <irinawebdev@gmail.com>
2025-11-12 22:51:08 +0200

    Update App.js

--- training-app/App.js
+++ training-app/App.js
@@ -27,6 +27,13 @@ export default function App() {
     setText("");
     console.log("Add task");
   };
+
+  const removeTask = (id) => {
+    Alert.alert('Delete task?', 'This cannot be undone',
+                [{text: 'Cancel', style: 'cancel'},
+                 {text: 'Delete', style: 'Delete', onPress: ()=> setTask{prev => prev.filter(t=> t.id != id)}]
+  };
+  
   return (
     <View style={styles.container}>
       <Text>Let's do it!</Text>
